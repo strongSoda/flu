5c086212f198f148b8d20bf7e1caadcf5d1ca5e7
strongSoda <strongsoda2@gmail.com>
2019-06-20 13:24:07 +0530

    deps | venv

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 import click
 
 @click.command()
-def hello():
-    click.echo('Hello, World!')
+@click.option('--name',prompt='Your good name?', default='World',help='your name')
+def hello(name):
+    """CLI to generate beautiful & optimised website scaffolds"""
+    click.echo('Hello, {}!'.format(name))
 
 if __name__ == '__main__':
     hello()
